64af2f95372b282496b3d68416ff48160c99e0aa
Vinicius de Lima Gomes <37003005+vinnilmg@users.noreply.github.com>
2019-03-18 20:44:06 -0300

    Tentando um rebase para o push funcionar

--- app/src/main/java/com/example/usjtads3anmcabusca_fila_resultado/ListaChamadosActivity.java
+++ app/src/main/java/com/example/usjtads3anmcabusca_fila_resultado/ListaChamadosActivity.java
@@ -10,6 +10,10 @@ import android.widget.ListView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+<<<<<<< HEAD
+=======
+import java.util.Date;
+>>>>>>> Conseitos OB & Refatoração das Views
 import java.util.List;
 
 public class ListaChamadosActivity extends AppCompatActivity {
@@ -24,14 +28,25 @@ public class ListaChamadosActivity extends AppCompatActivity {
         chamadosListView = findViewById(R.id.chamadosListView);
         Intent origemIntent = getIntent();
         String nomeFila = origemIntent.getStringExtra("nome_fila");
+<<<<<<< HEAD
         final List<String> chamados = busca(nomeFila);
         ArrayAdapter <String> adapter =
                 new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, chamados);
+=======
+        final List<Chamado> chamados = busca(nomeFila);
+        ArrayAdapter <Chamado> adapter =
+                new ArrayAdapter<>(this,
+                        android.R.layout.simple_list_item_1, chamados);
+>>>>>>> Conseitos OB & Refatoração das Views
         chamadosListView.setAdapter(adapter);
         chamadosListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+<<<<<<< HEAD
                 String chamadoSelecionado = chamados.get(position);
+=======
+                Chamado chamadoSelecionado = chamados.get(position);
+>>>>>>> Conseitos OB & Refatoração das Views
                 //Toast.makeText(ListaChamadosActivity.this, ChamadoSelecionado, Toast.LENGTH_SHORT).show();
                 Intent intent = new Intent(ListaChamadosActivity.this, DetalhesChamadoActivity.class);
                 intent.putExtra("chamado_selecionado", chamadoSelecionado);
@@ -40,6 +55,7 @@ public class ListaChamadosActivity extends AppCompatActivity {
         });
     }
 
+<<<<<<< HEAD
     public List<String> busca (String nomeFila){
         List <String> chamados = geraListaChamados();
         if (nomeFila == null || nomeFila.length() == 0 )
@@ -47,12 +63,23 @@ public class ListaChamadosActivity extends AppCompatActivity {
         List<String> resultado = new ArrayList<>();
         for (String chamado : chamados){
             if (chamado.toLowerCase().contains(nomeFila.toLowerCase())){
+=======
+    public List<Chamado> busca (String nomeFila){
+        List<Chamado> chamados = geraListaChamados();
+        if (nomeFila == null || nomeFila.length() == 0 )
+            return chamados;
+        List<Chamado> resultado = new ArrayList<>();
+        for (Chamado chamado : chamados){
+            if (chamado.getFila().getNome().toLowerCase().
+                    contains(nomeFila.toLowerCase())){
+>>>>>>> Conseitos OB & Refatoração das Views
                 resultado.add(chamado);
             }
         }
         return resultado;
     }
 
+<<<<<<< HEAD
 
 
 
@@ -82,4 +109,85 @@ public class ListaChamadosActivity extends AppCompatActivity {
         return lista;
     }
 
+=======
+    public List<String> geraListaChamados(){
+        List<String> lista = new ArrayList<>();
+        chamados.add(new Chamado(
+                new Fila(
+                        "Desktops",
+                        R.drawable.ic_computer_black_24dp
+                ),
+                "Computador da secretária quebrado.",
+                new Date(),
+                null,
+                "Aberto"
+        ));
+        chamados.add(new Chamado (
+                new Fila ("Telefonia",
+                        R.drawable.ic_phone_in_talk_black_24dp),
+                "Telefone não funciona.",
+                new Date(),
+                null,
+                "Aberto")
+        );
+        chamados.add(new Chamado (
+                new Fila ("Redes",
+                        R.drawable.ic_network_check_black_24dp),
+                "Manutenção no proxy.",
+                new Date(),
+                null,
+                "Aberto")
+        );
+        chamados.add(new Chamado (
+                new Fila ("Servidores", R.drawable.ic_poll_black_24dp),
+                "Lentidão generalizada.",
+                new Date(),
+                null,
+                "Aberto")
+        );
+        chamados.add(new Chamado (
+                new Fila ("Novos Projetos",
+                        R.drawable.ic_new_releases_black_24dp),
+                "CRM",
+                new Date(),
+                null,
+                "Aberto")
+        );
+        chamados.add(new Chamado (
+                new Fila ("Novos Projetos",
+                        R.drawable.ic_new_releases_black_24dp),
+                "Gestão de Orçamento",
+                new Date(),
+                null,
+                "Aberto")
+        );
+        chamados.add(new Chamado (
+                new Fila ("Redes",
+                        R.drawable.ic_network_check_black_24dp),
+                "Internet com lentidão",
+                new Date(),
+                null,
+                "Aberto")
+        );
+        chamados.add(new Chamado (
+                new Fila ("Novos Projetos",
+                        R.drawable.ic_new_releases_black_24dp),
+                "Chatbot",
+                new Date(),
+                null,
+                "Aberto")
+        );
+        chamados.add(new Chamado (
+                new Fila ("Novos Projetos",
+                        R.drawable.ic_new_releases_black_24dp),
+                "Chatbot",
+                new Date(),
+                null,
+                "Aberto")
+        );
+        return chamados;
+    }
+
+
+>>>>>>> Conseitos OB & Refatoração das Views
 }
